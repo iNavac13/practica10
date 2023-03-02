ab656d796dddf67b0fd6653142a4a5de1f3e8d7e
Luis Nava <luiseduardo@Felipes-MacBook-Pro.local>
2023-03-02 07:42:45 -0600

    Fixed

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ app.listen(port);
 app.get('/api', function (req, res) {
     res.json({firstname: 'John', lastname: 'Doe'})
 })
+
+app.get('/person/:id', function (req, res) {
+    res.json('<html><head></head><body>Person:' + req.params.id +'</body></html>')
+})
